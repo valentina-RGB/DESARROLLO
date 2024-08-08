3729b5397b11fa81c4149749a0be55f686c8a97e
Santiago Florez <santiagoflorezv23@gmail.com>
2024-08-07 19:26:13 -0500

    aa

--- APIV1/models/Clientes.js
+++ APIV1/models/Clientes.js
@@ -9,10 +9,37 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.CHAR(1),
       defaultValue: 'A',
     },
+    correo:{
+      type: DataTypes.FLOAT(100),
+      unique: true,
+      allowNull: true,
+    },
+    telefono: {
+      type: DataTypes.FLOAT(10),
+      defaultValue: '1234567899',
+      unique: true,
+      allowNull: true,
+    },
+    documento: {
+    type: DataTypes.FLOAT(15),
+    unique: true,
+    defaultValue:'123456789456',
+    },
+    nombre: {
+    type: DataTypes.STRING(100),
+    unique:true,
+    allowNull: true,
+    },
+    direccion: {
+      type: DataTypes.FLOAT(100),
+      unique:true,
+      allowNull: true,
+      },
+
   }, {
     tableName: 'Clientes',
     timestamps: false,
   });
 
-  return Clientes;
+  return Clientes;
 };
