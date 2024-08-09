734e281860a62ccadeec7237bbb65f72f8b97f9b
Santiago Florez <santiagoflorezv23@gmail.com>
2024-08-08 23:09:07 -0500

    ventas modelo

--- APIV1/models/ventas.js
+++ APIV1/models/ventas.js
@@ -5,22 +5,68 @@ module.exports = (sequelize, DataTypes) => {
             autoIncrement: true,
             primaryKey: true,
         },
-        //prueba
         fecha: {
             type: DataTypes.DATE,
             defaultValue: DataTypes.NOW,
             allowNull: false
         },
         ID_cliente: {
-            type: DataTypes.INTEGER,
+            type: DataTypes.INTEGER,module.exports = (sequelize, DataTypes) => {
+                const Ventas = sequelize.define('Ventas', {
+                    ID_venta: {
+                        type: DataTypes.INTEGER,
+                        autoIncrement: true,
+                        primaryKey: true,
+                    },
+                    //prueba
+                    fecha: {
+                        type: DataTypes.DATE,
+                        defaultValue: DataTypes.NOW,
+                        allowNull: false
+                    },
+                    ID_cliente: {
+                        type: DataTypes.INTEGER,
+                        allowNull: true,
+                        references: {
+                            model: 'Clientes',
+                            key: 'ID',
+                        },
+                        onUpdate: 'CASCADE',
+                        onDelete: 'SET NULL',
+                        comment: 'Por favor ingrese el cliente',
+                    },
+                    precio_total: {
+                        type: DataTypes.FLOAT,
+                        allowNull: true
+                    },
+                    ID_estado_venta: {
+                        type: DataTypes.INTEGER,
+                        references: {
+                            model: 'Estado_ventas',
+                            key: 'ID_estado_venta'
+                        },
+                    },
+                },{
+                    tableName: 'Ventas',
+                    timestamps: false,
+                });
+            
+                Ventas.associate = function(models) {
+                    Ventas.belongsTo(models.Clientes, { foreignKey: 'ID_cliente', as: 'Cliente' });
+                    Ventas.belongsTo(models.Estado_ventas, { foreignKey: 'ID_estado_venta' }); 
+                    Ventas.belongsToMany(models.Productos, { through: models.Producto_Ventas, foreignKey: 'ID_venta', otherKey: 'ID_producto' });
+                }
+            
+                return Ventas;
+            };
+            
             allowNull: true,
             references: {
                 model: 'Clientes',
-                key: 'ID',
+                key: 'ID_cliente',
             },
             onUpdate: 'CASCADE',
             onDelete: 'SET NULL',
-            comment: 'Por favor ingrese el cliente',
         },
         precio_total: {
             type: DataTypes.FLOAT,
@@ -39,8 +85,8 @@ module.exports = (sequelize, DataTypes) => {
     });
 
     Ventas.associate = function(models) {
-        Ventas.belongsTo(models.Clientes, { foreignKey: 'ID_cliente', as: 'Cliente' });
-        Ventas.belongsTo(models.Estado_ventas, { foreignKey: 'ID_estado_venta' }); 
+        Ventas.belongsTo(models.Clientes, { foreignKey: 'ID_cliente' });
+        Ventas.belongsTo(models.Estado_ventas, { foreignKey: 'ID_estado_venta' }); // Asociación inversa
         Ventas.belongsToMany(models.Productos, { through: models.Producto_Ventas, foreignKey: 'ID_venta', otherKey: 'ID_producto' });
     }
 
