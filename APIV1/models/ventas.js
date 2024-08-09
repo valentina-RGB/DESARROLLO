e854523ec45e0e8c520965e65ff4930b9b3d014a
Santiago Florez <santiagoflorezv23@gmail.com>
2024-08-08 23:12:49 -0500

    aaaaaa

--- APIV1/models/ventas.js
+++ APIV1/models/ventas.js
@@ -11,55 +11,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false
         },
         ID_cliente: {
-            type: DataTypes.INTEGER,module.exports = (sequelize, DataTypes) => {
-                const Ventas = sequelize.define('Ventas', {
-                    ID_venta: {
-                        type: DataTypes.INTEGER,
-                        autoIncrement: true,
-                        primaryKey: true,
-                    },
-                    //prueba
-                    fecha: {
-                        type: DataTypes.DATE,
-                        defaultValue: DataTypes.NOW,
-                        allowNull: false
-                    },
-                    ID_cliente: {
-                        type: DataTypes.INTEGER,
-                        allowNull: true,
-                        references: {
-                            model: 'Clientes',
-                            key: 'ID',
-                        },
-                        onUpdate: 'CASCADE',
-                        onDelete: 'SET NULL',
-                        comment: 'Por favor ingrese el cliente',
-                    },
-                    precio_total: {
-                        type: DataTypes.FLOAT,
-                        allowNull: true
-                    },
-                    ID_estado_venta: {
-                        type: DataTypes.INTEGER,
-                        references: {
-                            model: 'Estado_ventas',
-                            key: 'ID_estado_venta'
-                        },
-                    },
-                },{
-                    tableName: 'Ventas',
-                    timestamps: false,
-                });
-            
-                Ventas.associate = function(models) {
-                    Ventas.belongsTo(models.Clientes, { foreignKey: 'ID_cliente', as: 'Cliente' });
-                    Ventas.belongsTo(models.Estado_ventas, { foreignKey: 'ID_estado_venta' }); 
-                    Ventas.belongsToMany(models.Productos, { through: models.Producto_Ventas, foreignKey: 'ID_venta', otherKey: 'ID_producto' });
-                }
-            
-                return Ventas;
-            };
-            
+            type: DataTypes.INTEGER,
             allowNull: true,
             references: {
                 model: 'Clientes',
